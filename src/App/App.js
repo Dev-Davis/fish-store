95b7d1cd7a3b5cfca7ec20b184d409689bb7806c
Sean Davis <sean.dev.davis@gmail.com>
2019-06-24 18:52:38 -0500

    wip

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
+import Auth from '../Components/Auth/Auth';
+import Home from '../Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <button className='btn btn-danger'>HELP ME</button>
+      <Auth />
+      <Home />
     </div>
   );
 }
