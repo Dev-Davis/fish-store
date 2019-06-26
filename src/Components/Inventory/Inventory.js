9a6d980f6f613ed6b1c2c312e64621ab656fff53
Sean Davis <sean.dev.davis@gmail.com>
2019-06-25 20:53:17 -0500

    wip

--- src/Components/Inventory/Inventory.js
+++ src/Components/Inventory/Inventory.js
@@ -1,6 +1,7 @@
 /* eslint-disable eol-last */
 import React from 'react';
 
+import Fish from '../Fish/Fish';
 import fishData from '../../helpers/data/fishData';
 
 class Inventory extends React.Component {
@@ -15,9 +16,15 @@ class Inventory extends React.Component {
   }
 
   render() {
+    const fishComponents = this.state.fishes.map(fish => (
+      <Fish key={fish.id} fish={fish}/>
+    ))
     return (
       <div className="Inventory">
-        <h1>Inventory</h1>
+        <h2>Inventory</h2>
+        <ul className="fishes">
+          {fishComponents}
+        </ul>
       </div>
     );
   }
